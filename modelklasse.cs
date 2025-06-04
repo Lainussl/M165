d3e64b940d09732ef51e67394e1d6e189bc0b5cd
Lainussl <linus.lind@edu.gbssg.ch>
2025-06-04 20:23:59 +0200

    Create modelklasse.cs

---
+++ modelklasse.cs
@@ -0,0 +1,9 @@
+public class Item
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string Id { get; set; }
+
+    public string Name { get; set; }
+    public int Quantity { get; set; }
+}
